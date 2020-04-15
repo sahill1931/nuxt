47dfd4962a744f259ef5dcb3d85f89b2f622304e
sahil kaushal <sahil1931@gmail.com>
2020-04-15 14:04:28 +0530

    first commit

---
+++ client/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="container">
+    <div>
+      <h1>Home</h1>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+
+	mounted() {
+    this.isLogin();
+  },
+
+  data() {
+      return {
+          form: {
+              email: '',
+              password: ''
+          },
+      }
+  },
+  components: {
+    Logo
+  },
+
+  methods: {
+    isLogin() {
+       if( this.$router.history.current.query ) {
+          this.$auth.login({data: this.$router.history.current.query})
+          this.$router.push({name: 'index'});
+       }
+    },
+  },
+};
+</script>
